641f3965b369a859aac124ce024eab9132f7d164
theghost1980 <saturnob612@gmail.com>
2023-08-09 09:42:27 -0400

    added: drawer-based navigation minimal config & setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
-import {Box, NativeBaseProvider, Text} from 'native-base';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {NativeBaseProvider} from 'native-base';
 import React from 'react';
+import 'react-native-gesture-handler';
 import Form from './src/components/Form';
+import HistoryInvoices from './src/components/HistoryInvoices';
+
+const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+      {/* <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
         <Text>Welcome to your Keychain Powered store</Text>
         <Form />
-      </Box>
+      </Box> */}
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={Form} />
+          <Drawer.Screen name="History Invoices" component={HistoryInvoices} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ src/components/HistoryInvoices.tsx
@@ -0,0 +1,9 @@
+import {Text, VStack} from 'native-base';
+
+export default () => {
+  return (
+    <VStack>
+      <Text>History Screen</Text>
+    </VStack>
+  );
+};
